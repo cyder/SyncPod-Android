edd5bf5dceab8a2162541610f0b2c7199a258ff8
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-04 00:01:25 +0900

    [add] MainActivityからVideoActivityに遷移

--- app/src/main/java/com/example/atsushi/youtubesync/MainActivity.java
+++ app/src/main/java/com/example/atsushi/youtubesync/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.atsushi.youtubesync;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((Button) findViewById(R.id.startButton))
+            .setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                Intent varIntent =
+                        new Intent(MainActivity.this, VideoActivity.class);
+                startActivity(varIntent);
+            }
+        });
     }
 }
