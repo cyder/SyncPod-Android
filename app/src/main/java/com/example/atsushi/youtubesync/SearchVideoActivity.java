e0872c2ea426abc760d7e9f87c20f69371e01b05
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-11 02:28:06 +0900

    [add] ビデオ検索画面のひな形作成

---
+++ app/src/main/java/com/example/atsushi/youtubesync/SearchVideoActivity.java
@@ -0,0 +1,16 @@
+package com.example.atsushi.youtubesync;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by atsushi on 2017/10/11.
+ */
+
+public class SearchVideoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_video);
+    }
+}
--- app/src/main/java/com/example/atsushi/youtubesync/VideoActivity.java
+++ app/src/main/java/com/example/atsushi/youtubesync/VideoActivity.java
@@ -1,5 +1,7 @@
 package com.example.atsushi.youtubesync;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -38,6 +40,15 @@ public class VideoActivity extends YouTubeFailureRecoveryActivity implements Roo
 
         videoTitleText = (TextView) findViewById(R.id.video_title);
         playList = (LinearLayout) findViewById(R.id.play_list);
+
+        ((FloatingActionButton) findViewById(R.id.add_video_action_button))
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v){
+                        Intent varIntent = new Intent(VideoActivity.this, SearchVideoActivity.class);
+                        startActivity(varIntent);
+                    }
+                });
     }
 
     @Override
