e3a538d1a22cea6e324a773925dfc6f0e115f516
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-12 01:44:33 +0900

    [fix] 検索実行時にキーボードを非表示に

--- app/src/main/java/com/example/atsushi/youtubesync/SearchVideoActivity.java
+++ app/src/main/java/com/example/atsushi/youtubesync/SearchVideoActivity.java
@@ -1,5 +1,6 @@
 package com.example.atsushi.youtubesync;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -75,6 +77,8 @@ public class SearchVideoActivity extends AppCompatActivity implements SearchInte
                 Log.d("App", "Submit");
                 if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                     search.get(youtubeSearchForm.getText().toString());
+                    InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
                     return true;
                 }
                 return false;
