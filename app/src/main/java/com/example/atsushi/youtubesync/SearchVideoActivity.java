f4f21f2862d5469b4b9661f7c4e48a7fb9d8ff5c
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-11 19:49:36 +0900

    [add] toolbarに検索窓を表示

--- app/src/main/java/com/example/atsushi/youtubesync/SearchVideoActivity.java
+++ app/src/main/java/com/example/atsushi/youtubesync/SearchVideoActivity.java
@@ -3,7 +3,10 @@ package com.example.atsushi.youtubesync;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,6 +21,17 @@ public class SearchVideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_video);
+        this.getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.youtube_search_tool_bar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         youtubeVideoIdForm = (EditText) findViewById(R.id.youtube_video_id_form);
 
         ((Button) findViewById(R.id.submit))
