203287aa80b56e4861c4d593a8e7d79830ad0e94
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-25 09:06:40 +0900

    [add] ActionCableのユーザ認証実装

---
+++ app/src/main/java/com/example/atsushi/youtubesync/MySelf.java
@@ -0,0 +1,13 @@
+package com.example.atsushi.youtubesync;
+
+/**
+ * Created by atsushi on 2017/10/25.
+ */
+
+public final class MySelf {
+    private static String token = "1:m-_SMCVWTnjxgAF2au4_";
+
+    public static String getToken() {
+        return token;
+    }
+}
