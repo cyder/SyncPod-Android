b128157a4ab15123dfd3764e7a6f2bcb7d47e33d
Kazuki Chigita <kazuki.chigichan@gmail.com>
2017-10-30 00:04:17 +0900

    [change] Gsonを用いるクラスはJsonPrameterを継承するように変更．

---
+++ app/src/main/java/com/example/atsushi/youtubesync/json_data/JsonParameter.java
@@ -0,0 +1,10 @@
+package com.example.atsushi.youtubesync.json_data;
+
+/**
+ * Created by chigichan24 on 2017/10/29.
+ */
+
+public abstract class JsonParameter extends NullPointerException {
+    public JsonParameter() {
+    }
+}
---
+++ app/src/main/java/com/example/atsushi/youtubesync/json_data/Response.java
@@ -0,0 +1,11 @@
+package com.example.atsushi.youtubesync.json_data;
+
+/**
+ * Created by atsushi on 2017/10/26.
+ */
+
+public class Response extends JsonParameter {
+    public String result;
+    public User user;
+    public Room room;
+}
