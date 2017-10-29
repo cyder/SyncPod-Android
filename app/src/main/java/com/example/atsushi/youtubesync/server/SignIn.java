a9cbd0ec6354b52051aeca3793890a6ba5b2aff7
Kazuki Chigita <kazuki.chigichan@gmail.com>
2017-10-30 01:12:07 +0900

    [change] SignInをPostクラスを介してできるように変更した．

--- app/src/main/java/com/example/atsushi/youtubesync/server/SignIn.java
+++ app/src/main/java/com/example/atsushi/youtubesync/server/SignIn.java
@@ -1,56 +1,34 @@
 package com.example.atsushi.youtubesync.server;
 
-import android.util.Log;
-
-import com.example.atsushi.youtubesync.json_data.Result;
-import com.example.atsushi.youtubesync.json_data.SignInParam;
-import com.google.gson.Gson;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.example.atsushi.youtubesync.json_data.JsonParameter;
+import com.example.atsushi.youtubesync.json_data.Response;
 
 /**
  * Created by atsushi on 2017/10/27.
  */
 
-public class SignIn {
+public class SignIn extends Post {
     private SignInInterface listener = null;
 
-    public void setListener(SignInInterface _listener){
+    public SignIn() {
+        super();
+    }
+
+    public void setListener(SignInInterface _listener) {
         listener = _listener;
     }
 
-    public void post(final String email, final String password) {
-        new Thread(new Runnable() {
+    @Override
+    protected void post(JsonParameter jsonParameter, String endPoint, PostCallback callback) {
+        super.post(jsonParameter, endPoint, new PostCallback() {
             @Override
-            public void run() {
-                try {
-                    Gson gson = new Gson();
-                    SignInParam param = new SignInParam(email, password);
-                    URL url = new URL("http://59.106.220.89:3000/api/v1/login");
-                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                    con.setRequestMethod("POST");
-                    con.setRequestProperty("Content-Type", "application/json; charset=utf-8");
-                    OutputStream os = con.getOutputStream();
-                    PrintStream ps = new PrintStream(os);
-                    ps.print(gson.toJson(param));
-                    ps.close();
-
-                    BufferedReader reader =
-                            new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
-                    String buffer = reader.readLine();
-                    Result result = gson.fromJson(buffer, Result.class);
-                    if(result.result != null && result.result.equals("success")) {
-                        listener.onSignedIn(result.user);
-                    }
-                } catch(Exception e) {
-                    Log.e("App", "There was an IO error: " + e.getCause() + " : " + e.getMessage());
-                }
+            public void call(Response response) {
+                listener.onSignedIn(response.user);
             }
-        }).start();
+        });
+    }
+
+    public void post(final String email, final String password) {
+        post(new com.example.atsushi.youtubesync.json_data.SignIn(email, password), "login", null);
     }
 }
