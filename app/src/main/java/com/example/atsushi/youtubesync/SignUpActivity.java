424aff52a24d6db26b21f628fc2bfb4248b8619a
Kazuki Chigita <kazuki.chigichan@gmail.com>
2017-11-01 03:50:06 +0900

    [fix] SignUpに関する部分を切り出した．

---
+++ app/src/main/java/com/example/atsushi/youtubesync/SignUpActivity.java
@@ -0,0 +1,67 @@
+package com.example.atsushi.youtubesync;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import com.example.atsushi.youtubesync.json_data.User;
+import com.example.atsushi.youtubesync.server.SignUp;
+import com.example.atsushi.youtubesync.server.SignUpInterface;
+
+/**
+ * Created by chigichan24 on 2017/11/01.
+ */
+
+public class SignUpActivity extends AppCompatActivity implements SignUpInterface {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_sign_up);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.sign_up_tool_bar);
+        toolbar.setTitle(R.string.register_account_title);
+        final SignUp signUp = new SignUp();
+        signUp.setListener(this);
+        final EditText emailForm = (EditText) findViewById(R.id.sign_up_email);
+        final EditText nameForm = (EditText) findViewById(R.id.sign_up_name);
+        final EditText passwordForm = (EditText) findViewById(R.id.sign_up_password);
+        final EditText passwordConfirmForm = (EditText) findViewById(R.id.sign_up_password_confirm);
+
+
+        findViewById(R.id.sign_up_submit)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String email = emailForm.getText().toString();
+                        String name = nameForm.getText().toString();
+                        String password = passwordForm.getText().toString();
+                        String passwordConfirm = passwordConfirmForm.getText().toString();
+                        if (password.equals(passwordConfirm)) {
+                            signUp.post(email, name, password);
+                        }
+                    }
+                });
+
+        findViewById(R.id.sign_up_link)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent varIntent =
+                                new Intent(SignUpActivity.this, SignInActivity.class);
+                        startActivityForResult(varIntent, SIGN_IN_REQUEST_CODE);
+                    }
+                });
+    }
+
+    @Override
+    public void onSignedUp(final User user) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                setToken(user.access_token);
+            }
+        });
+    }
+}
