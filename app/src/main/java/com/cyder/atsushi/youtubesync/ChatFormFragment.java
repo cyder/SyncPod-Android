4f62b5a38c3294935d0ae30d30e94dfe36a9ae87
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-11-29 03:45:47 +0900

    [add] animation実装

--- app/src/main/java/com/cyder/atsushi/youtubesync/ChatFormFragment.java
+++ app/src/main/java/com/cyder/atsushi/youtubesync/ChatFormFragment.java
@@ -3,11 +3,15 @@ package com.cyder.atsushi.youtubesync;
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.cyder.atsushi.youtubesync.animations.ResizeAnimation;
 
 /**
  * Created by atsushi on 2017/11/27.
@@ -54,9 +58,34 @@ public class ChatFormFragment extends Fragment {
         if (chatFormArea != null) {
             if (nowFragment instanceof ChatFragment) {
                 chatFormArea.setVisibility(View.VISIBLE);
+                final ResizeAnimation animation = new ResizeAnimation(chatFormArea, dpToPx(48), 0);
+                animation.setDuration(100);
+                chatFormArea.startAnimation(animation);
             } else {
-                chatFormArea.setVisibility(View.GONE);
+                final ResizeAnimation animation = new ResizeAnimation(chatFormArea, -chatFormArea.getHeight(), chatFormArea.getHeight());
+                animation.setDuration(100);
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation arg0) {
+                        chatFormArea.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                    }
+                });
+                chatFormArea.startAnimation(animation);
             }
         }
+    }
+
+    private int dpToPx(int dp) {
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+        int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+        return px;
     }
 }
