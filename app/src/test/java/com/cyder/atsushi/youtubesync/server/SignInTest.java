c319cbba3b37c92f622194eab82289679beeddaf
Kazuki Chigita <kazuki.chigichan@gmail.com>
2018-01-23 07:03:42 +0900

    [fix] Add correct response

--- app/src/test/java/com/cyder/atsushi/youtubesync/server/SignInTest.java
+++ app/src/test/java/com/cyder/atsushi/youtubesync/server/SignInTest.java
@@ -1,19 +1,15 @@
 package com.cyder.atsushi.youtubesync.server;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.util.EntityUtils;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.nio.charset.StandardCharsets;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.matching;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 
 /**
  * Created by chigichan24 on 2018/01/23.
@@ -23,15 +19,9 @@ public class SignInTest {
     public WireMockRule wireMockRule = new WireMockRule(8080);
     @Test
     public void post() throws Exception {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("http://localhost:8080/__admin");
-        org.apache.http.HttpResponse response = httpclient.execute(httpGet);
-
-        httpGet = new HttpGet("http://localhost:8080/login");
-        httpGet.setHeader(new BasicHeader("Accept","text/plain"));
-        response = httpclient.execute(httpGet);
-
-        assertThat(EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8), is("{\"result\":\"success\"}"));
+        stubFor(WireMock.post(urlPathEqualTo("/login"))
+                .withHeader("Content-Type",matching("application/json"))
+                .willReturn(aResponse()));
     }
 
 }
