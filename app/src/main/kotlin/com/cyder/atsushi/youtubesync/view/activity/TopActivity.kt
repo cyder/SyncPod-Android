15ac7501315e679947bdcff8582f55922f5693e2
Kazuki Chigita <kazuki.chigichan@gmail.com>
2018-02-22 03:05:04 +0900

    [add] Set view layout

--- app/src/main/kotlin/com/cyder/atsushi/youtubesync/view/activity/TopActivity.kt
+++ app/src/main/kotlin/com/cyder/atsushi/youtubesync/view/activity/TopActivity.kt
@@ -2,7 +2,10 @@ package com.cyder.atsushi.youtubesync.view.activity
 
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import com.cyder.atsushi.youtubesync.R
+import com.cyder.atsushi.youtubesync.databinding.ActivityTopBinding
 import com.cyder.atsushi.youtubesync.viewmodel.TopActivityViewModel
 import javax.inject.Inject
 
@@ -12,11 +15,14 @@ import javax.inject.Inject
 
 class TopActivity : BaseActivity() {
     @Inject lateinit var viewModel: TopActivityViewModel
+    private lateinit var binding: ActivityTopBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getComponent().inject(this)
         bindViewModel(viewModel)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_top)
+        binding.viewModel = viewModel
     }
     companion object {
         fun createIntent(context: Context): Intent = Intent(context, TopActivity::class.java)
