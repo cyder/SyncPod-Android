1f64fa8a2e1dcac7b58d638ce11e5aeac5486ccc
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-03-28 16:44:16 +0900

    [add] signup画面の項目を表示

--- app/src/main/kotlin/com/cyder/atsushi/youtubesync/view/activity/SignUpActivity.kt
+++ app/src/main/kotlin/com/cyder/atsushi/youtubesync/view/activity/SignUpActivity.kt
@@ -2,12 +2,30 @@ package com.cyder.atsushi.youtubesync.view.activity
 
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import com.cyder.atsushi.youtubesync.R
+import com.cyder.atsushi.youtubesync.databinding.ActivitySignUpBinding
+import com.cyder.atsushi.youtubesync.viewmodel.SignUpActivityViewModel
+import javax.inject.Inject
 
 /**
  * Created by atsushi on 2018/03/28.
  */
 
 class SignUpActivity : BaseActivity() {
+    @Inject lateinit var viewModel: SignUpActivityViewModel
+    private lateinit var binding: ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getComponent().inject(this)
+        bindViewModel(viewModel)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        binding.viewModel = viewModel
+    }
+
     companion object {
         fun createIntent(context: Context): Intent = Intent(context, SignUpActivity::class.java)
     }
