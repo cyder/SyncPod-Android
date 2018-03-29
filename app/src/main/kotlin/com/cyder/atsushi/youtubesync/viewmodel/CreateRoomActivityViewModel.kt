99a0f55fae7acca2795c4d1e892bb170d6952ae9
Mori Atsushi <e1237@s.akashi.ac.jp>
2018-03-29 15:00:00 +0900

    [add] create room activity のdatabindingを設定

--- app/src/main/kotlin/com/cyder/atsushi/youtubesync/viewmodel/CreateRoomActivityViewModel.kt
+++ app/src/main/kotlin/com/cyder/atsushi/youtubesync/viewmodel/CreateRoomActivityViewModel.kt
@@ -1,5 +1,7 @@
 package com.cyder.atsushi.youtubesync.viewmodel
 
+import android.databinding.ObservableField
+import com.cyder.atsushi.youtubesync.view.helper.Navigator
 import com.cyder.atsushi.youtubesync.viewmodel.base.ActivityViewModel
 import javax.inject.Inject
 
@@ -7,7 +9,11 @@ import javax.inject.Inject
  * Created by atsushi on 2018/03/29.
  */
 class CreateRoomActivityViewModel @Inject constructor(
+        private val navigator: Navigator
 ) : ActivityViewModel() {
+    var roomName: ObservableField<String?> = ObservableField()
+    var roomDescription: ObservableField<String?> = ObservableField()
+
     override fun onStart() {
     }
 
@@ -18,5 +24,10 @@ class CreateRoomActivityViewModel @Inject constructor(
     }
 
     override fun onStop() {
+    }
+
+    fun onBackButtonClicked() = navigator.closeActivity()
+
+    fun onSubmit() {
     }
 }
