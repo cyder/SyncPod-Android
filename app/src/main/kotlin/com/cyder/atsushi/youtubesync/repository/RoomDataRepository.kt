5fd116b2c7ceb633dbdec2c5d42d46dd72b4ae08
Kazuki Chigita <kazuki.chigichan@gmail.com>
2018-04-06 03:35:27 +0900

    [clean] Extract consumer creating logic from repository

--- app/src/main/kotlin/com/cyder/atsushi/youtubesync/repository/RoomDataRepository.kt
+++ app/src/main/kotlin/com/cyder/atsushi/youtubesync/repository/RoomDataRepository.kt
@@ -4,7 +4,6 @@ import com.cyder.atsushi.youtubesync.api.SyncPodApi
 import com.cyder.atsushi.youtubesync.api.mapper.toModel
 import com.cyder.atsushi.youtubesync.api.request.CreateRoom
 import com.cyder.atsushi.youtubesync.model.Room
-import com.hosopy.actioncable.ActionCable
 import com.hosopy.actioncable.Channel
 import com.hosopy.actioncable.Consumer
 import com.hosopy.actioncable.Subscription
@@ -12,7 +11,6 @@ import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import java.net.URI
 import javax.inject.Inject
 
 /**
@@ -20,22 +18,22 @@ import javax.inject.Inject
  */
 class RoomDataRepository @Inject constructor(
         private val syncPodApi: SyncPodApi,
+        private val consumer: Consumer,
         private val token: String
 ) : RoomRepository {
+    lateinit var subscription: Subscription
     override fun joinRoom(roomKey: String): Completable {
-        CableConnection.provideConsumer(token)
-        CableConnection.provideSubscription(roomKey)
-
-        CableConnection.provideConsumer().connect()
-
+        val channel = Channel(CHANNEL_NAME, mapOf(ROOM_KEY to roomKey))
+        subscription = consumer.subscriptions.create(channel)
+        consumer.connect()
         return Completable.create { emitter ->
-            CableConnection.provideSubscription().onConnected = {
+            subscription.onConnected = {
                 emitter.onComplete()
             }
-            CableConnection.provideSubscription().onRejected = {
+            subscription.onRejected = {
                 emitter.onError(CannotJoinRoomException())
             }
-            CableConnection.provideSubscription().onFailed = {
+            subscription.onFailed = {
                 emitter.onError(it)
             }
         }
@@ -43,8 +41,8 @@ class RoomDataRepository @Inject constructor(
 
     override fun createNewRoom(name: String, description: String): Single<Room> {
         return syncPodApi.createNewRoom(token, CreateRoom(name, description))
-                .map{ it.room }
-                .map{ it.toModel() }
+                .map { it.room }
+                .map { it.toModel() }
                 .subscribeOn(Schedulers.computation())
                 .observeOn(AndroidSchedulers.mainThread())
     }
@@ -70,31 +68,8 @@ class RoomDataRepository @Inject constructor(
     }
 
     companion object {
-        const val WS_URL = "ws://59.106.220.89:3000/cable/"
         const val CHANNEL_NAME = "RoomChannel"
         const val ROOM_KEY = "room_key"
-        const val TOKEN = "token"
-    }
-
-    object CableConnection {
-        private var consumer: Consumer? = null
-        private var subscription: Subscription? = null
-        fun provideConsumer(token: String? = null): Consumer {
-            if(consumer == null && token != null) {
-                val options = Consumer.Options()
-                options.connection.query = mapOf(TOKEN to token)
-                consumer = ActionCable.createConsumer(URI(WS_URL),options)
-            }
-            return consumer as Consumer
-        }
-
-        fun provideSubscription(roomKey: String? = null): Subscription{
-            if(subscription == null && roomKey != null) {
-                val channel = Channel(CHANNEL_NAME, mapOf(ROOM_KEY to roomKey))
-                subscription = consumer?.subscriptions?.create(channel)
-            }
-            return subscription as Subscription
-        }
     }
 
     internal class CannotJoinRoomException : Exception("can not join room because of banned or mistook key")
