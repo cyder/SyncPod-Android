1c64eaf9ee546cd57a8446e911c180d5fe758dc7
Mori Atsushi <mori@cyder.tech>
2018-04-21 20:49:38 +0900

    [clear] リファクタ

--- app/src/main/kotlin/com/cyder/atsushi/youtubesync/repository/YouTubeDataRepository.kt
+++ app/src/main/kotlin/com/cyder/atsushi/youtubesync/repository/YouTubeDataRepository.kt
@@ -3,6 +3,7 @@ package com.cyder.atsushi.youtubesync.repository
 import com.cyder.atsushi.youtubesync.api.SyncPodApi
 import com.cyder.atsushi.youtubesync.api.mapper.toModel
 import com.cyder.atsushi.youtubesync.model.Video
+import com.cyder.atsushi.youtubesync.util.NotFilledFormsException
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.Singles
@@ -14,25 +15,28 @@ class YouTubeDataRepository @Inject constructor(
         private val syncPodApi: SyncPodApi,
         private val token: String
 ) : YouTubeRepository {
-    private var nextToken: BehaviorSubject<String> = BehaviorSubject.createDefault("")
-    private var keyword: BehaviorSubject<String> = BehaviorSubject.createDefault("")
+    private var nextToken: BehaviorSubject<String> = BehaviorSubject.create()
+    private var keyword: BehaviorSubject<String> = BehaviorSubject.create()
 
     override fun getYouTubeSearch(keyword: String): Single<List<Video>> {
-        return Single.just(keyword)
-                .filter { it.isNotBlank() }
-                .toSingle()
-                .doOnSuccess {
-                    this.keyword.onNext(it)
-                }
-                .flatMap {
-                    getYouTubeSearch(it, null)
-                }
+        this.keyword = BehaviorSubject.create()
+        this.nextToken = BehaviorSubject.create()
+
+        return Single.create<String> { emitter ->
+            if (keyword.isNotBlank()) {
+                emitter.onSuccess(keyword)
+            } else {
+                emitter.onError(NotFilledFormsException())
+            }
+        }.doOnSuccess {
+            this.keyword.onNext(it)
+        }.flatMap {
+            getYouTubeSearch(it, null)
+        }
     }
 
     override fun getNextYouTubeSearch(): Single<List<Video>> {
-        return Singles.zip(keyword.first(""), nextToken.first(""))
-                .filter { it.first.isNotBlank() && it.second.isNotBlank() }
-                .toSingle()
+        return Singles.zip(keyword.firstOrError(), nextToken.firstOrError())
                 .flatMap {
                     getYouTubeSearch(it.first, it.second)
                 }
@@ -44,7 +48,7 @@ class YouTubeDataRepository @Inject constructor(
                     it.nextPageToken?.apply {
                         nextToken.onNext(this)
                     } ?: apply {
-                        nextToken.onNext("")
+                        nextToken.onComplete()
                     }
                 }
                 .map { it.items }
