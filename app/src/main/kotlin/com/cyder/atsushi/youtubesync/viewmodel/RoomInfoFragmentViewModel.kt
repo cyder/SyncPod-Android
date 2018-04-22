5fb83e3c60249edb6152fe8ba471da89a7d03ef9
Mori Atsushi <mori@cyder.tech>
2018-04-22 14:03:49 +0900

    [add] ルーム情報を取得するようにした

--- app/src/main/kotlin/com/cyder/atsushi/youtubesync/viewmodel/RoomInfoFragmentViewModel.kt
+++ app/src/main/kotlin/com/cyder/atsushi/youtubesync/viewmodel/RoomInfoFragmentViewModel.kt
@@ -1,7 +1,9 @@
 package com.cyder.atsushi.youtubesync.viewmodel
 
+import android.databinding.ObservableBoolean
 import com.cyder.atsushi.youtubesync.repository.RoomRepository
 import com.cyder.atsushi.youtubesync.viewmodel.base.FragmentViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
 import javax.inject.Inject
 
 
@@ -12,16 +14,34 @@ class RoomInfoFragmentViewModel @Inject constructor(
         private val repository: RoomRepository
 ) : FragmentViewModel() {
     lateinit var roomKey: String
+    var isLoading: ObservableBoolean = ObservableBoolean()
+
     override fun onStart() {
     }
 
     override fun onResume() {
+        getRoom()
     }
 
     override fun onPause() {
     }
 
     override fun onStop() {
+    }
+
+    fun onRefresh() {
+        isLoading.set(true)
+        getRoom()
+    }
+
+    private fun getRoom() {
+        repository.fetch(roomKey)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ response ->
+                    isLoading.set(false)
+                }, {
+                    isLoading.set(false)
+                })
     }
 
 }
