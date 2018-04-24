2713a6e6ac8a87c72c1984504de2ad1c16aeb991
Mori Atsushi <mori@cyder.tech>
2018-04-24 15:29:57 +0900

    [add] オンラインメンバーの名前をタッチできるようにした

--- app/src/main/kotlin/com/cyder/atsushi/youtubesync/viewmodel/UserViewModel.kt
+++ app/src/main/kotlin/com/cyder/atsushi/youtubesync/viewmodel/UserViewModel.kt
@@ -1,9 +1,14 @@
 package com.cyder.atsushi.youtubesync.viewmodel
 
 import android.databinding.ObservableField
+import android.util.Log
 import com.cyder.atsushi.youtubesync.model.User
 import javax.inject.Inject
 
 class UserViewModel @Inject constructor(
         val user: ObservableField<User>
-)
+) {
+    fun onClick() {
+        Log.d("App", user.get().name)
+    }
+}
