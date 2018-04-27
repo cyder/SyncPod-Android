7a6add4b3f946b742af4330270a0f7b1d0d513a1
Mori Atsushi <mori@cyder.tech>
2018-04-27 18:31:02 +0900

    [fix] PlayListFragmentのinitを削除

--- app/src/main/kotlin/com/cyder/atsushi/youtubesync/viewmodel/PlayListFragmentViewModel.kt
+++ app/src/main/kotlin/com/cyder/atsushi/youtubesync/viewmodel/PlayListFragmentViewModel.kt
@@ -10,7 +10,9 @@ import com.cyder.atsushi.youtubesync.model.Video
 import com.cyder.atsushi.youtubesync.repository.VideoRepository
 import com.cyder.atsushi.youtubesync.view.helper.Navigator
 import com.cyder.atsushi.youtubesync.viewmodel.base.FragmentViewModel
+import io.reactivex.BackpressureStrategy
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.subjects.PublishSubject
 import javax.inject.Inject
 
 
@@ -23,6 +25,7 @@ class PlayListFragmentViewModel @Inject constructor(
         private val navigator: Navigator
 ) : FragmentViewModel() {
     lateinit var resources: Resources
+    private val onPause = PublishSubject.create<Unit>()
     val videoViewModels: ObservableList<VideoViewModel> = ObservableArrayList()
     val nowPlayVideo: ObservableField<Video> = ObservableField()
     val published: ObservableField<String> = ObservableField()
@@ -30,8 +33,12 @@ class PlayListFragmentViewModel @Inject constructor(
     val isPlaying: ObservableBoolean = ObservableBoolean(false)
     val hasPlayList: ObservableBoolean = ObservableBoolean(true)
 
-    init {
+    override fun onStart() {
+    }
+
+    override fun onResume() {
         repository.playListObservable
+                .takeUntil(onPause.toFlowable(BackpressureStrategy.LATEST))
                 .map { convertToViewModel(it) }
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
@@ -41,6 +48,7 @@ class PlayListFragmentViewModel @Inject constructor(
                 }
 
         repository.observeNowPlayingVideo()
+                .takeUntil(onPause.toFlowable(BackpressureStrategy.LATEST))
                 .subscribe {
                     nowPlayVideo.set(it)
                     published.set(resources.getString(R.string.published).format(it.published))
@@ -48,18 +56,14 @@ class PlayListFragmentViewModel @Inject constructor(
                 }
 
         repository.observeIsPlaying()
+                .takeUntil(onPause.toFlowable(BackpressureStrategy.LATEST))
                 .subscribe {
                     isPlaying.set(it)
                 }
     }
 
-    override fun onStart() {
-    }
-
-    override fun onResume() {
-    }
-
     override fun onPause() {
+        onPause.onNext(Unit)
     }
 
     override fun onStop() {
