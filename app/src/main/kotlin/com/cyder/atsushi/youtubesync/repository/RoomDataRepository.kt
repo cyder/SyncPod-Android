f402a345f71955681c46ca6a156f619c0a5b0526
Mori Atsushi <mori@cyder.tech>
2018-04-29 17:48:14 +0900

    [delete] 不要なimport文を削除

--- app/src/main/kotlin/com/cyder/atsushi/youtubesync/repository/RoomDataRepository.kt
+++ app/src/main/kotlin/com/cyder/atsushi/youtubesync/repository/RoomDataRepository.kt
@@ -7,8 +7,6 @@ import com.cyder.atsushi.youtubesync.model.Room
 import com.cyder.atsushi.youtubesync.model.User
 import com.cyder.atsushi.youtubesync.util.NotFilledFormsException
 import com.cyder.atsushi.youtubesync.websocket.SyncPodWsApi
-import com.cyder.atsushi.youtubesync.websocket.SyncPodWsApiImpl
-import com.hosopy.actioncable.Consumer
 import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
