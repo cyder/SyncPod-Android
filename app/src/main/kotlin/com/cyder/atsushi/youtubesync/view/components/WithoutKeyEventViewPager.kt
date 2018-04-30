09ef5dc728a9cd7b4c42694bd10d1dc598ad57af
Mori Atsushi <mori@cyder.tech>
2018-04-30 11:57:47 +0900

    [fix] キーボードの左右キーでタブが移動しないようにした

---
+++ app/src/main/kotlin/com/cyder/atsushi/youtubesync/view/components/WithoutKeyEventViewPager.kt
@@ -0,0 +1,12 @@
+package com.cyder.atsushi.youtubesync.view.components
+
+import android.content.Context
+import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.view.KeyEvent
+
+class WithoutKeyEventViewPager(context: Context, attributeSet: AttributeSet): ViewPager(context, attributeSet) {
+    override fun executeKeyEvent(event: KeyEvent): Boolean {
+        return false
+    }
+}
