dbcd5a155be3d849b85a3f5591dc66fd711b0dce
Shikibu <shikibu9419@gmail.com>
2018-05-08 00:29:23 +0900

    [add] ご意見フォームのベースを作成

--- app/src/main/kotlin/com/cyder/android/syncpod/repository/ContactRepository.kt
+++ app/src/main/kotlin/com/cyder/android/syncpod/repository/ContactRepository.kt
@@ -1,5 +1,13 @@
 package com.cyder.android.syncpod.repository
 
+import android.support.annotation.CheckResult
+import io.reactivex.Completable
+
 /**
  * Created by shikibu on 2018/05/07.
  */
+
+interface ContactRepository {
+    @CheckResult
+    fun sendContact(message: String): Completable
+}
--- app/src/main/kotlin/com/cyder/android/syncpod/view/activity/ContactActivity.kt
+++ app/src/main/kotlin/com/cyder/android/syncpod/view/activity/ContactActivity.kt
@@ -1,7 +1,40 @@
 package com.cyder.android.syncpod.view.activity
 
+import android.content.Context
+import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import com.cyder.android.syncpod.R
+import com.cyder.android.syncpod.databinding.ActivityContactBinding
+import com.cyder.android.syncpod.view.helper.setUpSnackbar
+import com.cyder.android.syncpod.viewmodel.ContactActivityViewModel
+import javax.inject.Inject
+
 /**
  * Created by shikibu on 2018/05/03.
  */
-class ContactActivity {
+
+class ContactActivity: BaseActivity() {
+    @Inject lateinit var viewModel: ContactActivityViewModel
+    private lateinit var binding: ActivityContactBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getComponent().inject(this)
+        bindViewModel(viewModel)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_contact)
+        binding.viewModel = viewModel
+
+        viewModel.callback = setUpSnackbar()
+    }
+
+    override fun onDestroy() {
+        viewModel.callback = null
+        super.onDestroy()
+    }
+
+    companion object {
+        fun createIntent(context: Context): Intent = Intent(context, ContactActivity::class.java)
+    }
 }
